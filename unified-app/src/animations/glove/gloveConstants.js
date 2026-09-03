a2caf55c4f39434636fe670e74f78191fbba0f45
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-09-03 15:58:25 +0400

    refactor: add GloVe objective constants

---
+++ unified-app/src/animations/glove/gloveConstants.js
@@ -0,0 +1,13 @@
+export const GLOVE_WEIGHTING_DEFAULTS = Object.freeze({
+  xMax: 100,
+  alpha: 0.75,
+  residual: 2,
+});
+
+export const GLOVE_COUNT_OPTIONS = Object.freeze([0, 1, 10, 100, 1000, 10000]);
+
+export const GLOVE_RESIDUAL_LIMITS = Object.freeze({
+  min: -4,
+  max: 4,
+  step: 0.25,
+});
