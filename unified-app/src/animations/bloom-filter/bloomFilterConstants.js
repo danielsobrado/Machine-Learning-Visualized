7a0c2ed6ff3192633747eb19d9f55f2bae95d9ab
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-09-04 10:33:17 +0400

    refactor: define Bloom filter teaching defaults

---
+++ unified-app/src/animations/bloom-filter/bloomFilterConstants.js
@@ -0,0 +1,11 @@
+export const BLOOM_DEFAULTS = {
+  size: 8,
+  protectedItem: 'Ant',
+  deletedItem: 'Cat',
+};
+
+export const BLOOM_TUNING_DEFAULTS = {
+  bits: 100,
+  expectedItems: 10,
+  hashes: 3,
+};
