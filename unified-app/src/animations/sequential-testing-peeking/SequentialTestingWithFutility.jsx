2b375b65563d25f80a76bcaa5a18a2e77d0fa8fb
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-09-13 17:27:43 +0400

    Compose sequential testing lesson with futility lab

---
+++ unified-app/src/animations/sequential-testing-peeking/SequentialTestingWithFutility.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SequentialTestingPeekingAnimation from './index.jsx';
+import FutilityStoppingLab from './FutilityStoppingLab.jsx';
+
+export default function SequentialTestingWithFutility() {
+  return (
+    <>
+      <SequentialTestingPeekingAnimation />
+      <FutilityStoppingLab />
+    </>
+  );
+}
