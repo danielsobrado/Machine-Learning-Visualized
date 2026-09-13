7dd892110648bb10d26b65e3c78d34e1eba00994
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-09-13 16:40:08 +0400

    Compose power lesson with multiplicity planning

---
+++ unified-app/src/animations/power-sample-size/PowerSampleSizeWithMultiplicity.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PowerSampleSizeAnimation from './index.jsx';
+import MultiplicityPlanningLab from './MultiplicityPlanningLab.jsx';
+
+export default function PowerSampleSizeWithMultiplicity() {
+  return (
+    <>
+      <PowerSampleSizeAnimation />
+      <div className="nb-lesson">
+        <MultiplicityPlanningLab />
+      </div>
+    </>
+  );
+}
