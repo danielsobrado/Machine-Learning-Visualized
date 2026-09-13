44311d0b41e000d102c2782943cc837839536d6f
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-09-13 06:55:30 +0400

    Lead neural network lesson with guided fundamentals

--- unified-app/src/animations/neural-network/EnhancedNeuralNetwork.jsx
+++ unified-app/src/animations/neural-network/EnhancedNeuralNetwork.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import BeginnerPath from './BeginnerPath.jsx';
 import LegacyNeuralNetwork from './index';
 import ExpressivityLab from './ExpressivityLab';
 
 export default function EnhancedNeuralNetwork() {
   return (
     <>
+      <BeginnerPath />
       <LegacyNeuralNetwork />
       <ExpressivityLab />
     </>
