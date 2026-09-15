224c313d4fad4f46ae9753bf7449e772fd3f8f7b
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-09-15 06:30:54 +0400

    Add RoPE model constants

---
+++ unified-app/src/animations/rope/ropeConstants.js
@@ -0,0 +1,6 @@
+export const ROTARY_DIMENSIONS = Object.freeze([32, 64, 128]);
+export const ROPE_BASES = Object.freeze([1000, 10000, 100000]);
+export const BASE_QUERY_PAIR = Object.freeze([0.92, 0.38]);
+export const BASE_KEY_PAIR = Object.freeze([0.86, -0.22]);
+export const MAX_POSITION = 32;
+export const MAX_VISIBLE_PAIRS = 8;
