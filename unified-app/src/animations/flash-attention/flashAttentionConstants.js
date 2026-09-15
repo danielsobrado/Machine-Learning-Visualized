5b4ffb80a258593a6b492afaa044cd8f34e718ce
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-09-15 06:00:09 +0400

    refactor(flash-attention): separate lesson constants

---
+++ unified-app/src/animations/flash-attention/flashAttentionConstants.js
@@ -0,0 +1,7 @@
+export const SEQUENCE_OPTIONS = Object.freeze([1024, 2048, 4096, 8192]);
+export const TILE_OPTIONS = Object.freeze([32, 64, 128, 256]);
+export const DTYPE_BYTES = Object.freeze({
+  fp32: 4,
+  fp16: 2,
+  bf16: 2,
+});
